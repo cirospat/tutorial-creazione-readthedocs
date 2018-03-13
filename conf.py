3234520d2d1ffd1fe69e32227c232b43ad05f925
ciro <cirospat@gmail.com>
2018-03-13 17:31:48 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # -- PROJECT Variables ----------------------------------------------------
-settings_project_name = "Titolo del documento"
-settings_copyright_copyleft = 'AgID - Team Digitale'
-settings_editor_name = 'AgID - Team Digitale'
+settings_project_name = "Linee guida open data comune Palermo"
+settings_copyright_copyleft = 'comune Palermo'
+settings_editor_name = 'Ciro Spataro'
 settings_doc_version = 'version: latest'
 settings_doc_release = 'version: latest'
-settings_basename = 'titolo-documento'
-settings_file_name = 'titolo-documento'
+settings_basename = 'linee-guida-open-data-palermo'
+settings_file_name = 'linee-guida-open-data-palermo'
 
 # -- No need to change below here
 
