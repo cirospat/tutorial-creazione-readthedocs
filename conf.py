b8b64e5180326dee0a8a00f28feed471564cb211
ciro <cirospat@gmail.com>
2018-03-13 21:04:13 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -8,7 +8,7 @@ settings_editor_name = 'Ciro Spataro'
 settings_doc_version = 'version: latest'
 settings_doc_release = 'version: latest'
 settings_basename = 'linee-guida-open-data-palermo'
-settings_file_name = 'linee-guida-open-data-palermo'
+settings_file_name = 'index'
 
 # -- No need to change below here
 
