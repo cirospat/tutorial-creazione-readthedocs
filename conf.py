afa0effd3533372da04dd26be834fd83720eb796
ciro <cirospat@gmail.com>
2018-03-19 07:09:12 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ settings_copyright_copyleft = 'comune Palermo'
 settings_editor_name = 'Ciro Spataro'
 settings_doc_version = 'version: latest'
 settings_doc_release = 'version: latest'
-settings_basename = 'regolamento-beni-confiscati-proprieta-comune-palermo'
+settings_basename = 'regolamento-sui-beni-confiscati-comune-palermo'
 settings_file_name = 'index'
 
 # -- No need to change below here
