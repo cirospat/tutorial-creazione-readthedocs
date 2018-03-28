8ce4cce495876d1f1ac4c7187e91fea231502fc6
ciro <cirospat@gmail.com>
2018-03-28 16:54:03 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -236,7 +236,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = "img/ods-small.png"
+#latex_logo = "images/..."
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
